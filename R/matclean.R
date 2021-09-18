ffc2a176a15c38306889303d11075c27d8c59ad4
Dilinie Seimon <dilinieseimon@gmail.com>
2021-09-18 17:24:46 +1000

    Changed matclean return list values

--- R/matclean.R
+++ R/matclean.R
@@ -25,44 +25,40 @@ matclean <- function(x, y) {
 
   if (missing(y)) {
     xname <- "Observation_number"
-    yname <- "y"
-    group <- "Group"
+    yname <- "x"
     x$Observation_number <- 1:nrow(x)
-    data <- pivotlonger(x, names_to = group, values_to = yname, ncolx + 1)
+    data <- pivotlonger(x, names_to = "Group", values_to = yname, ncolx + 1)
   } else if (ncolx > ncoly & ncoly == 1) {
     xname <- "x"
     yname <- colnames(y)
-    group <- "Group"
     data <- data.frame(x, y)
     ncol <- ncol(data)
     yname <- colnames(data)[ncol]
     data$Observation_number <- 1:nrow(data)
-    data <- pivotlonger(data, names_to = group, values_to = xname, c(ncol, (ncol + 1)))
+    data <- pivotlonger(data, names_to = "Group", values_to = xname, c(ncol, (ncol + 1)))
   } else if (ncoly > ncolx & ncolx == 1) {
     xname <- colnames(x)
     yname <- "y"
-    group <- "Group"
     data <- data.frame(x, y)
     ncol <- ncol(data)
     data$Observation_number <- 1:nrow(data)
-    data <- pivotlonger(data, names_to = group, values_to = yname, c(1, (ncol + 1)))
+    data <- pivotlonger(data, names_to = "Group", values_to = yname, c(1, (ncol + 1)))
   } else if (ncolx == ncoly) {
     xname <- "x"
     yname <- "y"
-    group <- "Group"
     colnames(x) <- colnames(y) <- paste0("Column ", 1:ncolx)
-    x <- pivotlonger(x, names_to = group, values_to = xname)
+    x <- pivotlonger(x, names_to = "Group", values_to = xname)
     x <- x[with(x, order(Group)), ]
     x$Observation_number <- 1:nrow(x)
-    y <- pivotlonger(y, names_to = group, values_to = yname)
+    y <- pivotlonger(y, names_to = "Group", values_to = yname)
     y <- y[with(y, order(Group)), ]
     y$Observation_number <- 1:nrow(y)
     data <- merge(x, y, by = "Observation_number", all = TRUE)
-    names(data)[names(data) == paste0(group, ".x")] <- group
-    data <- subset(data, select = -c(get(paste0(group, ".y"))))
+    names(data)[names(data) == paste0("Group", ".x")] <- "Group"
+    data <- subset(data, select = -c(get(paste0("Group", ".y"))))
   } else {
     stop("`x`` and `y` must have only 1 or the same number of columns", call. = FALSE)
   }
 
-  return(list(data = data, xname = xname, yname = yname, group = group, ncolx = ncolx, ncoly = ncoly))
+  return(list(data = data, xname = xname, yname = yname))
 }

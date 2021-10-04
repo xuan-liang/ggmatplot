a2513e5003efb803a6a45d6fe2ba1f019a996391
Dilinie Seimon <dilinieseimon@gmail.com>
2021-10-04 21:17:08 +1100

    Added more ggmatplot parameter tests

--- tests/testthat/test_ggmatplot_parameters.R
+++ tests/testthat/test_ggmatplot_parameters.R
@@ -36,11 +36,57 @@ test_that("line types updated based on linetype values", {
   expect_error(ggmatplot(x, y, plot.type = "both", linetype = c(15, 12, 3, 5)), "Too many linetype values. Only 3 needed but 4 provided.")
 })
 
+test_that("plot axis limits are updated based on xlim and ylim parameters", {
+  # x axis limits only
+  expect_doppelganger("density plot with x axis limits", ggmatplot(y, plot.type = "density", xlim = c(3,4)))
+  # y axis limits only
+  expect_doppelganger("histogram with y axis limits", ggmatplot(y, plot.type = "histogram", ylim = c(0,20)))
+  # x and y axis limits
+  expect_doppelganger("scatterplot with a and y axis limits", ggmatplot(x,y, plot.type = "point", xlim = c(5,7),ylim = c(0,3)))
+})
+
+test_that("plot axes are transformed to a log scale based on log parameter values", {
+  # log(x)
+  expect_doppelganger("line plot with log transformed x axis", ggmatplot(x,y, plot.type = "line", log = "x"))
+  # log(y)
+  expect_doppelganger("scatterplot with log transformed y axis", ggmatplot(x,y, plot.type = "point", log = "y"))
+  # log(x) and log(y)
+  expect_doppelganger("point + line plot with log transformed x and y axis", ggmatplot(x,y, plot.type = "both", log = "xy"))
+})
+
+test_that("plot title is updated based on main paramter", {
+  expect_equal(ggmatplot(x,y, plot.type = "line")$labels$title, NULL)
+  expect_equal(ggmatplot(x,y, plot.type = "line", main="title test")$labels$title, "title test")
+})
+
+test_that("plot axis labels are updated based on xlab and ylab paramters", {
+  expect_equal(ggmatplot(x,y, plot.type = "line", xlab="xlab test")$labels$x, "xlab test")
+  expect_equal(ggmatplot(y, plot.type = "point", ylab="ylab test")$labels$y, "ylab test")
+})
+
+
 test_that("invalid parameter values throw errors", {
   # invalid shape value
   expect_error(ggmatplot(x, y, plot.type = "point", shape = "red"), "ERROR")
   # invalid shape value
   expect_error(ggmatplot(x, y, plot.type = "line", linetype = "red"), "ERROR")
+  # invalid limits
+  expect_error(ggmatplot(y, plot.type = "line", xlim = c(-4,a)), "ERROR")
+  expect_error(ggmatplot(y, plot.type = "line", xlim = 5), "ERROR")
+  # density plot with y axis limits
+  expect_error(ggmatplot(y, plot.type = "density", ylim = c(3,4)), "ERROR")
+  # invalid log value
+  expect_error(ggmatplot(y, plot.type = "scatterplot", log = 1), "ERROR")
+
+  # only first value is considered
+  #ggmatplot(y, plot.type = "density", log = c("x","y"))
+
+
+  # negative axis values transformed using log scale
+  #z <- iris[, 1]-5
+  #ggmatplot(z,y, plot.type = "point",log = "x")
+
+  # log transformations on density, histogram y axis
 })
 
 test_that("unknown parameter values for plots throw errors", {

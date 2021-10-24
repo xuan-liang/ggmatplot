08e1167bef9e1f6a0d20b8ef59e8c2b92d230a08
Dilinie Seimon <dilinieseimon@gmail.com>
2021-10-24 13:27:23 +1100

    Validated ggmatplot parameters

--- R/ggmatplot.R
+++ R/ggmatplot.R
@@ -2,9 +2,7 @@
 #'
 #'
 #' `ggmatplot` is a quick and easy way of plotting two matrices against each
-#' other. Can also be used as an alternative to pivoting data into long format
-#' before plotting, by using the index column(s) and the columns to be plotted
-#' against the index as the two matrix inputs for `ggmatplot()`.
+#' other.
 #'
 #' @param x,y Vectors or matrices of data for plotting.
 #'
@@ -63,17 +61,21 @@
 #' ggmatplot(iris_sub[, c(1, 3)], iris_sub[, c(2, 4)])
 #' # Modify legend label and axis
 #' ggmatplot(iris_sub[, c(1, 3)], iris_sub[, c(2, 4)], shape = c(4, 6), legend_label = c("Sepal", "Petal"), legend_title = "", xlab = "Length", ylab = "Width")
-#'
-#'
-ggmatplot <- function(x, y, color = NULL, shape = NULL, linetype = NULL, fill = NULL,
-                      xlim = c(NA, NA), ylim = c(NA, NA), log = "",
-                      main = NULL, xlab = NULL, ylab = NULL,
+ggmatplot <- function(x, y, color = NULL, shape = NULL, linetype = NULL,
+                      fill = NULL, xlim = c(NA, NA), ylim = c(NA, NA),
+                      log = NULL, main = NULL, xlab = NULL, ylab = NULL,
                       legend_label = NULL, legend_title = NULL,
                       plot_type = "point", asp = NA, ...) {
-  if (!plot_type %in% c("point", "line", "both", "density", "histogram", "boxplot", "dotplot", "errorplot", "violin", "ecdf")) {
+
+  # valid plot types
+  if (!plot_type %in% c(
+    "point", "line", "both", "density", "histogram",
+    "boxplot", "dotplot", "errorplot", "violin", "ecdf"
+  )) {
     stop("plot_type can not take this value", call. = FALSE)
   }
   if (!missing(x) & !missing(y)) {
+    # only single matrix input allowed for the following plot types
     if (plot_type %in% c("density", "histogram", "boxplot", "violin")) {
       stop("This plot type only uses a single matrix input", call. = FALSE)
     }
@@ -204,16 +206,19 @@ ggmatplot <- function(x, y, color = NULL, shape = NULL, linetype = NULL, fill = 
   # number of unique groups
   numGroups <- length(unique(data$Group))
 
+  # default legend title
   if (is.null(legend_title)) legend_title <- "Group"
 
   if (!is.null(legend_label)) {
     # values > number of unique groups
     if (length(legend_label) > numGroups) {
-      stop(paste0("Too many legend_label values. Only ", numGroups, " needed but ", length(legend_label), " provided."), call. = FALSE)
+      stop(paste0("Too many legend_label values. Only ", numGroups, " needed but
+                  ", length(legend_label), " provided."), call. = FALSE)
     }
     # values < number of unique groups
     else if (length(legend_label) < numGroups) {
-      stop(paste0("Insufficient legend_label values. ", numGroups, " needed but only ", length(legend_label), " provided."), call. = FALSE)
+      stop(paste0("Insufficient legend_label values. ", numGroups, " needed but
+                  only ", length(legend_label), " provided."), call. = FALSE)
     }
   } else {
     legend_label <- unique(data$Group)
@@ -221,17 +226,29 @@ ggmatplot <- function(x, y, color = NULL, shape = NULL, linetype = NULL, fill = 
 
   if (!is.null(color)) {
     color <- validateNumParams(color, numGroups)
-    p <- p + scale_color_manual(name = legend_title, labels = legend_label, values = color)
+    p <- p + scale_color_manual(
+      name = legend_title, labels = legend_label,
+      values = color
+    )
     if (is.null(fill)) {
-      p <- p + scale_fill_manual(name = legend_title, labels = legend_label, values = color)
+      p <- p + scale_fill_manual(
+        name = legend_title, labels = legend_label,
+        values = color
+      )
     }
   }
 
   if (!is.null(fill)) {
     fill <- validateNumParams(fill, numGroups)
-    p <- p + scale_fill_manual(name = legend_title, labels = legend_label, values = fill)
+    p <- p + scale_fill_manual(
+      name = legend_title, labels = legend_label,
+      values = fill
+    )
     if (is.null(color)) {
-      p <- p + scale_color_manual(name = legend_title, labels = legend_label, values = fill)
+      p <- p + scale_color_manual(
+        name = legend_title, labels = legend_label,
+        values = fill
+      )
     }
   }
 
@@ -242,24 +259,53 @@ ggmatplot <- function(x, y, color = NULL, shape = NULL, linetype = NULL, fill = 
   }
 
   if (!is.null(shape)) {
-    shape <- validateNumParams(shape, numGroups)
-    p <- p + scale_shape_manual(name = legend_title, labels = legend_label, values = shape)
+    # shape parameter is only valid for the following plot types
+    if (plot_type %in% c("point", "both")) {
+      shape <- validateNumParams(shape, numGroups)
+    } else {
+      warning("Ignoring unknown parameters: shape", call. = FALSE)
+    }
+    p <- p + scale_shape_manual(
+      name = legend_title, labels = legend_label,
+      values = shape
+    )
   } else {
     p <- p + scale_shape_discrete(name = legend_title, labels = legend_label)
   }
 
   if (!is.null(linetype)) {
-    linetype <- validateNumParams(linetype, numGroups)
-    p <- p + scale_linetype_manual(name = legend_title, labels = legend_label, values = linetype)
+    # linetype parameter is invalid for the following plot types
+    if (!plot_type %in% c("point")) {
+      linetype <- validateNumParams(linetype, numGroups)
+    } else {
+      warning("Ignoring unknown parameters: linetype", call. = FALSE)
+    }
+    p <- p + scale_linetype_manual(
+      name = legend_title, labels = legend_label,
+      values = linetype
+    )
   } else {
     p <- p + scale_linetype_discrete(name = legend_title, labels = legend_label)
   }
 
   if (!is.null(main)) p <- p + ggtitle(main)
 
-  logv <- function(var) var %in% strsplit(log, "")[[1]]
-  if (logv("x")) p <- p + scale_x_log10() + xlab(paste0("log(", p$labels$x, ")"))
-  if (logv("y")) p <- p + scale_y_log10() + ylab(paste0("log(", p$labels$y, ")"))
+  if (!is.null(log)) {
+    # validating list of values the log parameter can take
+    if (!log %in% c("x", "y", "xy")) {
+      stop("log can not take this value", call. = FALSE)
+    } else {
+      logv <- function(var) var %in% strsplit(log, "")[[1]]
+      if (logv("x")) {
+        p <- p + scale_x_log10() +
+          xlab(paste0("log(", p$labels$x, ")"))
+      }
+      if (logv("y")) {
+        p <- p + scale_y_log10() +
+          ylab(paste0("log(", p$labels$y, ")"))
+      }
+    }
+  }
 
   if (!is.null(xlab)) p <- p + xlab(xlab)
   if (!is.null(ylab)) p <- p + ylab(ylab)

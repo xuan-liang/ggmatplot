36787f069adbb0352777c8b839194b2a8e3c08ef
Dilinie Seimon <dilinieseimon@gmail.com>
2021-11-21 18:00:32 +1100

    Added errorplot stats tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,6 @@
 library(testthat)
-library(tibble)
 library(ggmatplot)
+library(tibble)
+library(stats)
 
 test_check("ggmatplot")
---
+++ tests/testthat/test_errorplotstats.R
@@ -0,0 +1,39 @@
+iris_sub <- subset(iris, Species == "setosa")[1:4]
+data <- ggmatplot::matclean(iris_sub)$data
+
+
+errorstats_mean_se <- errorplotstats(data, desc_stat = "mean_se")
+errorstats_mean_sd <- errorplotstats(data, desc_stat = "mean_sd")
+errorstats_median_iqr <- errorplotstats(data, desc_stat = "median_iqr")
+errorstats_median_range <- errorplotstats(data, desc_stat = "median_range")
+
+test_that("format of resultant data frame is as expected", {
+  expect_named(errorstats, c("Group", "y", "ymin", "ymax"))
+  expect_setequal(errorstats$Group, colnames(iris_sub))
+})
+
+test_that("column means are calculated as expected", {
+  suppressWarnings(expect_setequal(errorstats_mean_se$y, colMeans(iris_sub)))
+})
+
+test_that("column medians are calculated as expected", {
+  suppressWarnings(expect_setequal(errorstats_median_iqr$y, sapply(iris_sub, median)))
+})
+
+test_that("column errors bars are calculated as expected", {
+  # when error bars use standard errors
+  suppressWarnings(expect_setequal(errorstats_mean_se$ymin, (sapply(iris_sub, mean)-(sapply(iris_sub, sd)/sqrt(nrow(iris_sub))))))
+  suppressWarnings(expect_setequal(errorstats_mean_se$ymax, (sapply(iris_sub, mean)+(sapply(iris_sub, sd)/sqrt(nrow(iris_sub))))))
+
+  # when error bars use standard deviations
+  suppressWarnings(expect_setequal(errorstats_mean_sd$ymin, (sapply(iris_sub, mean)-(sapply(iris_sub, sd)))))
+  suppressWarnings(expect_setequal(errorstats_mean_sd$ymax, (sapply(iris_sub, mean)+(sapply(iris_sub, sd)))))
+
+  # when error bars use interquartile range
+  suppressWarnings(expect_setequal(errorstats_median_iqr$ymin, sapply(iris_sub, quantile, probs = 0.25)))
+  suppressWarnings(expect_setequal(errorstats_median_iqr$ymax, sapply(iris_sub, quantile, probs = 0.75)))
+
+  # when error bars use range
+  suppressWarnings(expect_setequal(errorstats_median_range$ymin, sapply(iris_sub, min)))
+  suppressWarnings(expect_setequal(errorstats_median_range$ymax, sapply(iris_sub, max)))
+})
